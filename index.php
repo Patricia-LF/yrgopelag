69698f0752bbfad2cd1808398b9fedb36a7ddbed
Patricia Loayza Frykberg <patricia_lf_86@hotmail.com>
2025-12-12 21:56:54 +0100

    Added carousel

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require __DIR__ . '/header.php';
+
+?>
+
+<div class="carousel-container" id="carousel">
+    <!-- Slides kommer att genereras av JavaScript -->
+</div>
+
+<script src="script.js"></script>
