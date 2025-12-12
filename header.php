a10702c793f532dc5fe4cba7ff564e7608afe40d
Patricia Loayza Frykberg <patricia_lf_86@hotmail.com>
2025-12-12 11:24:06 +0100

    Added some new files

---
+++ header.php
@@ -0,0 +1,27 @@
+<?php
+
+require __DIR__ . '/data.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="footer.css">
+    <title>Yrgopelag</title>
+</head>
+
+<body>
+
+    <header>
+        <h1>Pixel Hotel</h1>
+    </header>
+
+</body>
+
+</html>
