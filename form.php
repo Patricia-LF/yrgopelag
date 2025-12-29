7f7dbe7b807348eeff1c62613067f6e185bb825f
Patricia Loayza Frykberg <patricia_lf_86@hotmail.com>
2025-12-29 14:48:48 +0100

    Update form.php

--- form.php
+++ form.php
@@ -6,8 +6,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-/* require __DIR__ . '/config.php'; */
-require __DIR__ . '/header.php';
+require __DIR__ . '/config.php';
+/* require __DIR__ . '/header.php'; */
 
 // Hotellägarinfo - centralbank )
 define('HOTEL_OWNER_USER', 'Patricia ');
