d6c63b984037db959d2a1ecc603d8f06066470f1
Patricia Loayza Frykberg <patricia_lf_86@hotmail.com>
2026-01-07 14:36:24 +0100

    updated footer

--- footer.php
+++ footer.php
@@ -1,8 +1,40 @@
 <footer>
-    <h1>Infinity hotel</h1>
-    <p>Contact</p>
-    <p>About</p>
-    <p>© 2025-2026 Patricia Loayza Frykberg</p>
+    <div class="footer-content">
+        <div class="footer-section">
+            <h1>Infinity Hotel</h1>
+            <p class="footer-tagline">Paradise on Isla Syntax</p>
+            <img src="images/infinity-logo.png" class="logo-img">
+        </div>
+
+        <div class="footer-section">
+            <h3>Contact</h3>
+            <p><img src="images/icons/phone-icon.png"> +1 (555) 727-2347</p>
+            <p><img src="images/icons/email-icon.png"> info@infinityhotel.sx</p>
+            <p>1 Oceanview Drive, Paradise Bay</p>
+            <p>Isla Syntax, PS 12345</p>
+        </div>
+
+        <div class="footer-section">
+            <h3>Services</h3>
+            <p>Restaurant: +1 (555) 348-3463</p>
+            <p>Events: +1 (555) 383-6871</p>
+            <p>Spa: +1 (555) 772-9355</p>
+            <p>Emergency: +1 (555) 911-ISLA</p>
+        </div>
+
+        <div class="footer-section">
+            <h3>Follow Us</h3>
+            <div class="social-icons">
+                <a href="#" aria-label="Instagram"><img src="images/icons/Instagram_icon.png"></a>
+                <a href="#" aria-label="Facebook"><img src="images/rooms/facebook-icon.png"></a>
+                <a href="#" aria-label="Twitter"><img src="images/icons/twitter-icon.png"></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-bottom">
+        <p>© 2025-2026 Patricia Loayza Frykberg</p>
+    </div>
 </footer>
 
 </body>
